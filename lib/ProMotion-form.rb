4686d6e8c864d361209d470fe66529bf7b776f16
Steve Shreeve <steve.shreeve@gmail.com>
2014-11-13 12:39:00 -0500

    does this order matter?

--- lib/ProMotion-form.rb
+++ lib/ProMotion-form.rb
@@ -7,8 +7,8 @@ require 'motion-cocoapods'
 Motion::Project::App.setup do |app|
   lib_dir_path = File.dirname(File.expand_path(__FILE__))
   app.files << File.join(lib_dir_path, "ProMotion/form/form_view_controller.rb")
-  app.files << File.join(lib_dir_path, "ProMotion/form/form.rb")
   app.files << File.join(lib_dir_path, "ProMotion/form/form_style.rb")
+  app.files << File.join(lib_dir_path, "ProMotion/form/form.rb")
   app.files << File.join(lib_dir_path, "ProMotion/form/form_screen.rb")
 
   app.pods do
