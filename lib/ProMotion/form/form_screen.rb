3bea990bf7ef9095a6ffdd8160d2eb8d2999061a
Steve Shreeve <steve.shreeve@gmail.com>
2014-11-15 01:39:11 -0500

    allow sticky styles to be defined and recalled

--- lib/ProMotion/form/form_screen.rb
+++ lib/ProMotion/form/form_screen.rb
@@ -36,6 +36,15 @@ module ProMotion
         one ? (all << @form_styles[one]) : all
       end
     end
+
+    def style!(*ary)
+      @form_style_sticky = ary.empty? ? nil : ary
+      style(*ary)
+    end
+
+    def style?(*ary)
+      ary.unshift(*@form_style_sticky) if @form_style_sticky
+      style(*ary)
     end
   end
 end
